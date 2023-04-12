221fc6f296532175408e8218cb416c2c7063b305
Kendrick Arnett <kmarnett@gmail.com>
2023-04-12 16:39:06 -0500

    One more time with feeling.

--- src/pages/storybook/react.js
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-export default function Component() {
-  if (typeof window !== "undefined") {
-    window.location.pathname = "/storybook/react/index.html";
-  }
-
-  return;
-}
